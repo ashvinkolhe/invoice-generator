83f8e15039649ec3201e796377bf2dd336235a4e
Ashwin Kolhe <89703923+ashvinkolhe@users.noreply.github.com>
2024-03-20 21:56:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import InvoiceForm from './components/InvoiceForm';
+
+function App() {
+  return (
+<div class="min-h-screen bg-gradient-to-b from-blue-500 to-black-800 animate-gradient">
+      <div className="mx-auto max-w-7xl">
+        <InvoiceForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
